cd6c2611411b8c37b874c9370e3faab0d6cef006
Dan Nolte <dnolte@hsph.harvard.edu>
2022-11-16 15:01:45 -0500

    update random_sampling.R to accept only lme4 models

--- R/random_sampling.R
+++ R/random_sampling.R
@@ -1,5 +1,5 @@
 generate_random_Beta_m = function(uids, mod){
-  Beta_hat = coef(summary(mod))[, 1]
+  Beta_hat = mod@beta
   Vcov_hat = vcov(mod)
 
   # Sample Beta_m
@@ -8,12 +8,12 @@ generate_random_Beta_m = function(uids, mod){
 }
 
 generate_random_beta_i_ms = function(uids, mod){
-  beta_i_hats = cbind(uids, ranef(mod))
+  beta_i_hats = cbind(uids, ranef(mod)[[1]])
+  vcov_beta_i = matrix(unclass(VarCorr(MAR_model))[[1]], 2, 2)
   beta_i_ms = hash()
   for (uid in uids) {
     beta_i_hat = as.numeric(beta_i_hats[which(uids == uid), 2:3])
-    vocv_beta_i = getVarCov(mod, individual = uid, type = "random.effects")
-    beta_i_m = mvrnorm(1, beta_i_hat, vocv_beta_i)
+    beta_i_m = mvrnorm(1, beta_i_hat, vcov_beta_i)
     beta_i_ms[[toString(uid)]] = beta_i_m
   }
   return(beta_i_ms)
