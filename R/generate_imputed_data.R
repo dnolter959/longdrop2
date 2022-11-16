3511b2d7e550ff3159ebc66dea4f020adad9fa98
Dan Nolte <dnolte@hsph.harvard.edu>
2022-11-16 15:00:39 -0500

    update generate_imputed_data.R to generalize implementation

--- R/generate_imputed_data.R
+++ R/generate_imputed_data.R
@@ -20,26 +20,31 @@ construct_u = function(uids, beta_i_ms) {
   return(matrix(u))
 }
 
-generate_complete_dataset_using_multiple_imputation = function(X, uids, timepoints, q, Beta_m, beta_i_ms, trace_of_hat_matrix, sigma_hat_squared){
+generate_complete_dataset_using_multiple_imputation = function(MAR_model, design_X_full, k, uids, timepoints, Beta_m, beta_i_ms){
+  q = length(unique(uids))
+  trace_of_hat_matrix = sum(hatvalues(MAR_model))
+  var_cor = as.data.frame(VarCorr(MAR_model))
+  sigma_hat_squared = var_cor[nrow(var_cor), ncol(var_cor)]^2
   n = length(timepoints)
   N = q*n
   Z = construct_Z(timepoints, q)
   u = construct_u(uids, beta_i_ms)
   R = rep(1, n*q)
   epsilon = calculate_epsilon(N, trace_of_hat_matrix, sigma_hat_squared)
-  imputed_values = X %*% Beta_m + Z %*% u + k*R + epsilon
+  imputed_values = design_X_full %*% Beta_m + Z %*% u + k*R + epsilon
 
   full_imputed_data = data.frame(
-    uid = sort(rep(uids, n)),
-    arm = X[, 2],
-    week = X[, 3],
+    uid = as.numeric(as.character(sort(rep(uids, n)))),
+    arm = design_X_full[, 2],
+    week = design_X_full[, 3],
     WHO5_imputed = as.vector(imputed_values)
-  ) %>% mutate(arm = factor(arm, levels = c(0, 1)))
+  )
   colnames(full_imputed_data) = c("uid", "arm", "week", "WHO5_imputed")
 
-  complete_dataset = left_join(full_imputed_data, data, by = c("uid", "week", "arm")) %>%
+  complete_dataset = left_join(full_imputed_data, MAR_model@frame, by = c("uid", "week")) %>%
     mutate(WHO5 = ifelse(is.na(WHO5), WHO5_imputed, WHO5)) %>%
-    dplyr::select(-WHO5_imputed) %>%
+    dplyr::select(-WHO5_imputed, -arm.y) %>%
+    dplyr::rename(arm = arm.x) %>%
     arrange(uid, week)
   return(complete_dataset)
 }
